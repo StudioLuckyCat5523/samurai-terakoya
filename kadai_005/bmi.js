d77ba6d7ef8aee687a117b421a5d8629a50858cd
StudioLuckyCat5523 <162400926+StudioLuckyCat5523@users.noreply.github.com>
2024-08-21 14:24:53 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+//身長
+let userHight;
+
+//値の代入
+userHight  = 1.7;
+console.log ('身長'+userHight+'M');
+
+//体重
+let userWeight;
+//値の代入
+userWeight = 68;
+console.log('体重'+userWeight+'Kg');　
+
+
+//BMI
+
+console.log ('BMIは'+(userWeight/(userHight*userHight))+'です');
+
+
